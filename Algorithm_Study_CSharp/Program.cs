14640bcc0f10cb2e9933f0da1e108ba345353b66
Sangmin <99625610+jsmin0131@users.noreply.github.com>
2023-08-02 17:06:01 +0900

    fixed untracked files

--- Algorithm_Study_CSharp/Program.cs
+++ Algorithm_Study_CSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Algorithm_Study_CSharp
@@ -10,7 +11,19 @@ namespace Algorithm_Study_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Thread t = new Thread(threadFunc);
+            t.IsBackground = true;
+            
+            t.Start();
+            t.Join();
+        }
+
+        static void threadFunc()
+        {
+            Console.WriteLine("5초 후에 프로그램 종료");
+            Thread.Sleep(1000 * 5);
+
+            Console.WriteLine("스레드 종료!");
         }
     }
 }
